1532c72318428ee678829b9b64fe76422d5067f5
Liam Allard <lallard13@gmail.com>
2020-06-08 12:26:24 -0700

    Slight tweaks to the fix

--- project1/src/components/UpdateUserComponent.tsx
+++ project1/src/components/UpdateUserComponent.tsx
@@ -85,20 +85,20 @@ export class UpdateUserComponent extends React.Component<IUpdateUserComponentPro
             role: this.state.newRole,
         }
         try{
-            if(fieldsToUpdate.password !== this.state.confirmPassword && this.props.loggedInUser?.role !== 'admin'){
-                throw new Error('Password does not match!')
+            let updatedUser: User;
+            if(this.props.loggedInUser?.role !== 'admin') {
+                if(fieldsToUpdate.password !== this.state.confirmPassword){
+                    throw new Error('Password does not match!')
+                }
                 for(let key in fieldsToUpdate) {
                     // @ts-ignore
                     if(fieldsToUpdate[key] === '') {
                         throw new Error('Please include all fields');
                     }
                 }
-            }
-            let updatedUser: User;
-            if(this.props.loggedInUser?.role === 'admin') {
-                updatedUser = await updateUser({userId: fieldsToUpdate.userId, role: fieldsToUpdate.role})
-            } else {
                 updatedUser = await updateUser(fieldsToUpdate);
+            } else {
+                updatedUser = await updateUser({userId: fieldsToUpdate.userId, role: fieldsToUpdate.role})
             }
             this.setState({
                 userDisplay: updatedUser,
